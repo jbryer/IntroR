e9c3337284de565ceea69be22f2d7df8ad522db3
Jason Bryer <jason@bryer.org>
2015-10-28 10:06:35 -0400

    Updated installation instructions.

--- Installation/Setup.r
+++ Installation/Setup.r
@@ -1,8 +1,10 @@
-pkgs <- c('Deducer', 'devtools', 'doBy', 'foreign', 'gdata', 'ggplot2',
-          'Hmisc', 'JGR', 'knitr', 'maps', 'mapdata',
-          'maptools', 'psych', 'RCurl', 'reshape', 'RODBC',
-          'roxygen2', 'seqinr', 'sm', 'sp', 'sqldf', 'survey', 'WriteXLS',
-          'XML', 'xtable')
+# List of packages to install
+pkgs <- c('Deducer', 'devtools', 'doBy', 'dplyr', 'foreign', 'gdata', 'ggplot2',
+		  'ggthemes','ggvis', 'haven', 'Hmisc', 'JGR', 'knitr','lubridate', 
+		  'maps', 'mapdata', 'maptools', 'psych', 'RCurl', 'readr', 'readxl',
+		  'reshape2', 'rmarkdown', 'RODBC', 'roxygen2', 'seqinr', 'shiny',
+		  'shinydashboards','sm', 'sp', 'sqldf', 'stringr', 'survey', 
+		  'WriteXLS', 'XML', 'xtable')
 
 .libPaths()[1] #This is where R will install the packages
 
@@ -12,7 +14,8 @@ for(l in pkgs) {
 	if(!is_installed(l)) {
 		if(Sys.info()['sysname'] == 'Windows') {
 			install.packages(l, dependencies=TRUE,
-				repos=c("http://www.stats.ox.ac.uk/pub/RWin", "http://cran.r-project.org"))
+				repos=c("http://www.stats.ox.ac.uk/pub/RWin", 
+						"http://cran.r-project.org"))
 		} else {
 			install.packages(l, dependencies=TRUE,
 							 repos=c("http://cran.r-project.org"))			
@@ -27,13 +30,18 @@ update.packages(ask=FALSE, repos='http://cran.r-project.org')
 #The following packages will be installed from github.com. The install_github
 #function is in the devtools package.
 require(devtools)
-install_github('retention','jbryer')
-install_github('irutils','jbryer')
-install_github('ipeds','jbryer')
-install_github('makeR','jbryer')
-install_github('pisa','jbryer')
-install_github('multilevelPSA','jbryer')
-install_github('TriMatch','jbryer')
+install_github('jbryer/likert')
+install_github('jbryer/sqlutils')
+install_github('jbryer/ipeds')
+install_github('jbryer/timeline')
+install_github('jbryer/retention')
+install_github('jbryer/DataCache')
+install_github('jbryer/makeR')
+install_github('jbryer/qualtrics')
+install_github('jbryer/multilevelPSA')
+install_github('jbryer/TriMatch')
+install_github('jbryer/PSAboot')
+install_github('jbryer/IS606')
 
 library() #Return list of installed packages
 search() #Return list of loaded packages
