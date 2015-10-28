afcb2fd6d75504f7b311247449013a7b93045040
Jason Bryer <jason@bryer.org>
2015-10-28 15:15:37 -0400

    Added openintro package.

--- Installation/Setup.r
+++ Installation/Setup.r
@@ -1,9 +1,9 @@
 # List of packages to install
 pkgs <- c('Deducer', 'devtools', 'doBy', 'dplyr', 'foreign', 'gdata', 'ggplot2',
 		  'ggthemes','ggvis', 'haven', 'Hmisc', 'JGR', 'knitr','lubridate', 
-		  'maps', 'mapdata', 'maptools', 'psych', 'RCurl', 'readr', 'readxl',
-		  'reshape2', 'rmarkdown', 'RODBC', 'roxygen2', 'seqinr', 'shiny',
-		  'sm', 'sp', 'sqldf', 'stringr', 'survey', 
+		  'maps', 'mapdata', 'maptools', 'openintro', 'psych', 'RCurl', 'readr', 
+		  'readxl', 'reshape2', 'rmarkdown', 'RODBC', 'roxygen2', 'seqinr', 
+		  'shiny', 'sm', 'sp', 'sqldf', 'stringr', 'survey', 
 		  'WriteXLS', 'XML', 'xtable')
 pkgs.github <- c('jbryer/likert', 'jbryer/sqlutils', 'jbryer/ipeds',
 				 'jbryer/timeline', 'jbryer/retention', 'jbryer/DataCache',
